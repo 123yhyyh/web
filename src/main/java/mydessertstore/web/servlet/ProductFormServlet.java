57a3701948e82f0de3f22916050f767adbdc3237
Olinta <3178922405@qq.com>
2022-11-06 10:32:39 +0800

    O

---
+++ src/main/java/mydessertstore/web/servlet/ProductFormServlet.java
@@ -0,0 +1,32 @@
+package main.java.mydessertstore.web.servlet;
+
+import main.java.mydessertstore.domain.Item;
+import main.java.mydessertstore.domain.Product;
+import main.java.mydessertstore.service.CatalogService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+public class ProductFormServlet extends HttpServlet {
+
+    private CatalogService catalogService;
+
+    private static final String PRODUCT_FORM = "/WEB-INF/jsp/catalog/product.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String productId = req.getParameter("productId");
+        catalogService = new CatalogService();
+        Product product = catalogService.getProduct(productId);
+        List<Item> itemList = catalogService.getItemListByProduct(productId);
+        HttpSession session = req.getSession();
+        session.setAttribute("product" , product);
+        session.setAttribute("itemList" , itemList);
+        req.getRequestDispatcher(PRODUCT_FORM).forward(req,resp);
+    }
+}
